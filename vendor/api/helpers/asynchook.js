f09d3df9e0fa1a80dc34007ce46a022a81488ac1
THINH <deverpham@gmail.com>
2018-10-21 19:05:15 +0700

    fix helper/asynchook.js

--- vendor/api/helpers/asynchook.js
+++ vendor/api/helpers/asynchook.js
@@ -1,14 +1,14 @@
 
 /**
  * Filter Bags for projects
- * @param {object} projects
+ * @param {any} default
  * @param {function} args
  * @return {Promise}
  */
-async function filterBags(projects, ...args) {
-    const newProjects = await args[0](projects);
+async function filterBags(defaultValue, ...args) {
+    const newValue = await args[0](defaultValue);
     const newArgs = args.slice(1);
-    if (newArgs.length > 0 && newProjects.length > 0) return await filterBags(newProjects, ...newArgs);
-    else return newProjects;
+    if (newArgs.length > 0 && newValue.length > 0) return await filterBags(newValue, ...newArgs);
+    else return newValue;
 }
 module.exports = { filterBags }
