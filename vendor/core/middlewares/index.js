e3062b4cfee9e3029e7e8e5656012b905b09d212
THINH <deverpham@gmail.com>
2018-10-25 19:13:20 +0700

    add_helpers

--- vendor/core/middlewares/index.js
+++ vendor/core/middlewares/index.js
@@ -2,9 +2,11 @@ const responseMiddleware = require('./response')
 const hookAPI = require('./hookapi')
 const bodyParserMiddleware = require('./bodyparser')
 const CookieMiddleware = require('./cookie');
+const viewHelperMiddleware = require('./viewhelper');
 module.exports = {
     responseMiddleware,
     hookAPI,
+    viewHelperMiddleware,
     bodyParserMiddleware,
     cookieMiddleware: CookieMiddleware()
 }
---
+++ vendor/core/middlewares/viewhelper.js
@@ -0,0 +1,25 @@
+const {
+    themeAPI,
+    loggerAPI
+} = require('../../api')
+const ejs = require('ejs');
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+module.exports = function (req, res, next) {
+    const helpers = glob.sync(themeAPI.getThemeHelperDir() + '/*.ejs');
+    helpers.map(helper => {
+        const name = path.basename(helper).replace('.ejs', '');
+        loggerAPI.debug(name)
+        const content = fs.readFileSync(helper).toString()
+        console.log(res.locals.hookAPI)
+        res.locals[name] = function (data) {
+            const dataMapLocals = Object.assign(res.locals, data)
+            return ejs.compile(content, {
+                async: true
+            })(dataMapLocals)
+        }
+
+    })
+    next();
+}
