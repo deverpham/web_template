d35bc40842d571bb5b269b1ee7a60b282c2db480
phuongnamvu17 <phuongnamvu17@gmail.com>
2018-10-25 13:03:54 +0700

    add jsdoc

--- vendor/api/helper.js
+++ vendor/api/helper.js
@@ -1,6 +1,10 @@
 const crypto = require('crypto');
 const configAPI = require('./config')
 class HelperAPI {
+    /**
+     * @param {string} encode - Encode type example: 'base64', 'hex'
+     * @param {string} text - Text that need to be encrypted
+     */
     encrypt(encode, text) {
         const salt = configAPI.database().secret_key
         return crypto.createHmac('sha256', salt)
